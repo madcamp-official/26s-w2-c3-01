c9c8f180e89c1e2495ab2619cca6e38b165ffaaf
batiger00 <batiger00@batiger00ui-MacBookPro.local>
2026-07-12 01:23:03 +0900

    feat(lounge): schedule stale membership cleanup

--- server/src/main/kotlin/com/melodybubble/server/MelodyBubbleServerApplication.kt
+++ server/src/main/kotlin/com/melodybubble/server/MelodyBubbleServerApplication.kt
@@ -2,8 +2,10 @@ package com.melodybubble.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class MelodyBubbleServerApplication
 
 fun main(args: Array<String>) {
